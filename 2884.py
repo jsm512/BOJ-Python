54a92e555adfeb663be712ba8d20aa7010326acc
jsm512 <wlstmdals12@naver.com>
2022-03-22 21:57:47 +0900

    Update 2884.py

--- 2884.py
+++ 2884.py
@@ -1,1 +1,12 @@
 h,m=map(int,input().split())
+
+if(h==0):
+    if(m>=45):
+        print(h,m-45)
+    else:
+        print(h+23,15+m)
+else:
+    if(m>=45):
+        print(h,m-45)
+    else:
+        print(h-1,15+m)
